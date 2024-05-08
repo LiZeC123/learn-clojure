9e385ea2f423667cac59d5f43afe8477580d242f
LiZeC <LiZeC123@users.noreply.github.com>
2024-05-08 05:30:45 +0000

    feat: 更新序列相关操作示例

--- loop.clj
+++ loop.clj
@@ -5,3 +5,26 @@
   )
 
 (println "Size is " (size [1 2 3 4]))
+
+
+
+; 列表操作
+(every? number? [1 2 3 :four])
+
+(filter (fn [x] (> x 4)) [1, 2, 3, 4, 5])
+
+(map (fn [x] (* x x)) [1 2 3])
+
+
+
+; 列表解析
+(def color ["red" "blue"])
+(for [x color] (str "I like " x))
+
+
+; 有穷序列与无穷序列
+
+(range 1 10)
+
+(take 5 (drop 2 (cycle [:i :love :clj])))
+(->> [:i :love :clj] (cycle) (drop 2) (take 5))
