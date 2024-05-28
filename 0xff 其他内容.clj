c4e2c3ea073850ac90f14d12af81894754b78dd4
LiZeC <LiZeC123@users.noreply.github.com>
2024-05-28 06:26:57 +0000

    feat: 更新笔记

--- 0xff 其他内容.clj
+++ 0xff 其他内容.clj
@@ -25,18 +25,11 @@
 
 
 
-; 列表操作
-(every? number? [1 2 3 :four])
-
-(filter (fn [x] (> x 4)) [1, 2, 3, 4, 5])
-
-(map (fn [x] (* x x)) [1 2 3])
 
 
 
-; 列表解析
-(def color ["red" "blue"])
-(for [x color] (str "I like " x))
+
+
 
 
 ; 有穷序列与无穷序列
